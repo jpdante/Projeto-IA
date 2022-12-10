113da58673fe2e2f6117a09c8822d34b9009a137
Nícolas Ribeiro Marques <56075706+NicolasRMarques@users.noreply.github.com>
2022-12-10 14:51:50 -0300

    Matriz de confusão (Esboço)

--- Servidor/Knn.py
+++ Servidor/Knn.py
@@ -1,6 +1,13 @@
 from Element import Element
 import numpy as np
 from typing import List
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy
+from sklearn import metrics
 
 class Knn:
 
@@ -17,7 +24,7 @@ class Knn:
         self.append(element)
 
     def euclideanDistance(self, element1, element2):
-        distance = np.linalg.norm(elementPosition - x.get_position())
+        distance = np.linalg.norm(element1.get_position() - element2.get_position())
 
     def moreFrequent(self, nearstNeighbors: List[Element]):
         mostFrequent = 0
@@ -34,4 +41,17 @@ class Knn:
 
     def append(self, element):
         self.elements.append(element)
+    def confusionMatrix(self, correctList):
+        correctListClasses = List[string]
+        elementsClasses = List[string]
+        for x in correctList:
+            correctListClasses.append(x.get_itsClass)
+        for x in self.elements:
+            elementsClasses.append(x.get_itsClass)
+        confusion_matrix = metrics.confusion_matrix(elementsClasses, correctListClasses)
+        cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=[False, True])
+        cm_display.plot()
+        plt.show()
+        plt.savefig(sys.stdout.buffer)
+        sys.stdout.flush()
 
